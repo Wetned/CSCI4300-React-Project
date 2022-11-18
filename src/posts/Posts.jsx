ff1fad76f044059defa0b003d65e918fbf364e45
az87346 <az87346@uga.edu>
2022-11-18 17:31:28 -0500

    added msg

--- src/posts/Posts.jsx
+++ src/posts/Posts.jsx
@@ -17,6 +17,7 @@ import "./posts.css"
       <div className="header">
         <div id="div_list">
         <h2>Add Classes</h2>
+        <p>Once logged in/signed up, this website allows you to add posts below so you can keep notes of classes you are interested in.</p>
         {/* <input type="text" name="item" id="usrInput"/> */}
         <button onClick={addComponent} id="addBtn" class="loggedIn">add</button>
         </div>
